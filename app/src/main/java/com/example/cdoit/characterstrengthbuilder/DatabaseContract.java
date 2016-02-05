bd0e97bac19418b588aba540bc87b02c9048631d
cddewitt <cddewitt@bsu.edu>
2016-02-04 19:56:47 -0500

    Created Database version 2

---
+++ app/src/main/java/com/example/cdoit/characterstrengthbuilder/DatabaseContract.java
@@ -0,0 +1,31 @@
+package com.example.cdoit.characterstrengthbuilder;
+
+/**
+ * Created by cdoit on 2/4/2016.
+ */
+public class DatabaseContract {
+
+    public static final String DB_NAME="GoalDataBase";
+
+    public static final class IncompleteGoals{
+        public static final String TABLENAME="IncompleteGoals";
+        public static final String COLUMN_ID="ID";
+        public static final String COLUMN_DATECREATED="DateCreated";
+        public static final String COLUMN_WISH="Wish";
+        public static final String COLUMN_OUTCOME="Outcome";
+        public static final String COLUMN_OBSTACLES="Obstacles";
+        public static final String COLUMN_PLAN="Plan";
+    }
+
+    public static final class CompleteGoals{
+        public static final String TABLENAME="CompleteGoals";
+        public static final String COLUMN_ID="ID";
+        public static final String COLUMN_DATECREATED="DateCreated";
+        public static final String COLUMN_DATECOMPLETED="DateCompleted";
+        public static final String COLUMN_WISH="Wish";
+        public static final String COLUMN_OUTCOME="Outcome";
+        public static final String COLUMN_OBSTACLES="Obstacles";
+        public static final String COLUMN_PLAN="Plan";
+    }
+
+}
--- app/src/main/java/com/example/cdoit/characterstrengthbuilder/MainActivity.java
+++ app/src/main/java/com/example/cdoit/characterstrengthbuilder/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.cdoit.characterstrengthbuilder;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
