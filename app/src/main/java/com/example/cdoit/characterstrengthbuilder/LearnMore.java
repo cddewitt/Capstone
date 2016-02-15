512fa4a435b324caaf147fee5201310d33ab1236
Jessica Stanford <jlstanford@bsu.edu>
2016-02-14 20:11:25 -0500

    Finished Learn More activity

---
+++ app/src/main/java/com/example/cdoit/characterstrengthbuilder/LearnMore.java
@@ -0,0 +1,34 @@
+package com.example.cdoit.characterstrengthbuilder;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LearnMore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_more);
+        Button linkButton = (Button) findViewById(R.id.linkButton);
+        linkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showWebPage();
+            }
+        });
+    }
+
+    void showWebPage() {
+        String urlString = "http://www.lovelythinking.com/";
+        Intent webIntent = new Intent(Intent.ACTION_VIEW);
+        webIntent.setData(Uri.parse(urlString));
+        startActivity(webIntent);
+    }
+
+}
+
+
