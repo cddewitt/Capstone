c6a8c8e227b244b5179ebfedd23624d5eacbdfaf
anroark <thealexroark@gmail.com>
2016-02-15 17:16:41 -0500

    Made the GritTestActivity class prettier, changed font sizes for WOOP details on the woop_review layout.

--- app/src/main/java/com/example/cdoit/characterstrengthbuilder/GritTestActivity.java
+++ app/src/main/java/com/example/cdoit/characterstrengthbuilder/GritTestActivity.java
@@ -45,6 +45,7 @@ public class GritTestActivity extends AppCompatActivity {
         item1TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item1TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item1TextViewParams.addRule(RelativeLayout.BELOW, gritTestTextView.getId());
+        item1TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item1TextView, item1TextViewParams);
 
         RadioGroup item1RadioGroup = new RadioGroup(this);
@@ -93,6 +94,7 @@ public class GritTestActivity extends AppCompatActivity {
         item1RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item1RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item1RadioGroupParams.addRule(RelativeLayout.BELOW, item1TextView.getId());
+        item1RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item1RadioGroup, item1RadioGroupParams);
 
         TextView item2TextView = new TextView(this);
@@ -103,6 +105,7 @@ public class GritTestActivity extends AppCompatActivity {
         item2TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item2TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item2TextViewParams.addRule(RelativeLayout.BELOW, item1RadioGroup.getId());
+        item2TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item2TextView, item2TextViewParams);
 
         RadioGroup item2RadioGroup = new RadioGroup(this);
@@ -151,6 +154,7 @@ public class GritTestActivity extends AppCompatActivity {
         item2RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item2RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item2RadioGroupParams.addRule(RelativeLayout.BELOW, item2TextView.getId());
+        item2RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item2RadioGroup, item2RadioGroupParams);
 
         TextView item3TextView = new TextView(this);
@@ -161,6 +165,7 @@ public class GritTestActivity extends AppCompatActivity {
         item3TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item3TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item3TextViewParams.addRule(RelativeLayout.BELOW, item2RadioGroup.getId());
+        item3TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item3TextView, item3TextViewParams);
 
         RadioGroup item3RadioGroup = new RadioGroup(this);
@@ -209,6 +214,7 @@ public class GritTestActivity extends AppCompatActivity {
         item3RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item3RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item3RadioGroupParams.addRule(RelativeLayout.BELOW, item3TextView.getId());
+        item3RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item3RadioGroup, item3RadioGroupParams);
 
         TextView item4TextView = new TextView(this);
@@ -219,6 +225,7 @@ public class GritTestActivity extends AppCompatActivity {
         item4TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item4TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item4TextViewParams.addRule(RelativeLayout.BELOW, item3RadioGroup.getId());
+        item4TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item4TextView, item4TextViewParams);
 
         RadioGroup item4RadioGroup = new RadioGroup(this);
@@ -267,6 +274,7 @@ public class GritTestActivity extends AppCompatActivity {
         item4RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item4RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item4RadioGroupParams.addRule(RelativeLayout.BELOW, item4TextView.getId());
+        item4RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item4RadioGroup, item4RadioGroupParams);
 
         TextView item5TextView = new TextView(this);
@@ -277,6 +285,7 @@ public class GritTestActivity extends AppCompatActivity {
         item5TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item5TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item5TextViewParams.addRule(RelativeLayout.BELOW, item4RadioGroup.getId());
+        item5TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item5TextView, item5TextViewParams);
 
         RadioGroup item5RadioGroup = new RadioGroup(this);
@@ -325,6 +334,7 @@ public class GritTestActivity extends AppCompatActivity {
         item5RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item5RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item5RadioGroupParams.addRule(RelativeLayout.BELOW, item5TextView.getId());
+        item5RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item5RadioGroup, item5RadioGroupParams);
 
         TextView item6TextView = new TextView(this);
@@ -335,6 +345,7 @@ public class GritTestActivity extends AppCompatActivity {
         item6TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item6TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item6TextViewParams.addRule(RelativeLayout.BELOW, item5RadioGroup.getId());
+        item6TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item6TextView, item6TextViewParams);
 
         RadioGroup item6RadioGroup = new RadioGroup(this);
@@ -383,6 +394,7 @@ public class GritTestActivity extends AppCompatActivity {
         item6RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item6RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item6RadioGroupParams.addRule(RelativeLayout.BELOW, item6TextView.getId());
+        item6RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item6RadioGroup, item6RadioGroupParams);
 
         TextView item7TextView = new TextView(this);
@@ -393,6 +405,7 @@ public class GritTestActivity extends AppCompatActivity {
         item7TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item7TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item7TextViewParams.addRule(RelativeLayout.BELOW, item6RadioGroup.getId());
+        item7TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item7TextView, item7TextViewParams);
 
         RadioGroup item7RadioGroup = new RadioGroup(this);
@@ -441,6 +454,7 @@ public class GritTestActivity extends AppCompatActivity {
         item7RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item7RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item7RadioGroupParams.addRule(RelativeLayout.BELOW, item7TextView.getId());
+        item7RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item7RadioGroup, item7RadioGroupParams);
 
         TextView item8TextView = new TextView(this);
@@ -451,6 +465,7 @@ public class GritTestActivity extends AppCompatActivity {
         item8TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item8TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item8TextViewParams.addRule(RelativeLayout.BELOW, item7RadioGroup.getId());
+        item8TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item8TextView, item8TextViewParams);
 
         RadioGroup item8RadioGroup = new RadioGroup(this);
@@ -499,6 +514,7 @@ public class GritTestActivity extends AppCompatActivity {
         item8RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item8RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item8RadioGroupParams.addRule(RelativeLayout.BELOW, item8TextView.getId());
+        item8RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item8RadioGroup, item8RadioGroupParams);
 
         TextView item9TextView = new TextView(this);
@@ -509,6 +525,7 @@ public class GritTestActivity extends AppCompatActivity {
         item9TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item9TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item9TextViewParams.addRule(RelativeLayout.BELOW, item8RadioGroup.getId());
+        item9TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item9TextView, item9TextViewParams);
 
         RadioGroup item9RadioGroup = new RadioGroup(this);
@@ -557,6 +574,7 @@ public class GritTestActivity extends AppCompatActivity {
         item9RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item9RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item9RadioGroupParams.addRule(RelativeLayout.BELOW, item9TextView.getId());
+        item9RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item9RadioGroup, item9RadioGroupParams);
 
         TextView item10TextView = new TextView(this);
@@ -567,6 +585,7 @@ public class GritTestActivity extends AppCompatActivity {
         item10TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item10TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item10TextViewParams.addRule(RelativeLayout.BELOW, item9RadioGroup.getId());
+        item10TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item10TextView, item10TextViewParams);
 
         RadioGroup item10RadioGroup = new RadioGroup(this);
@@ -615,6 +634,7 @@ public class GritTestActivity extends AppCompatActivity {
         item10RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item10RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item10RadioGroupParams.addRule(RelativeLayout.BELOW, item10TextView.getId());
+        item10RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item10RadioGroup, item10RadioGroupParams);
 
         TextView item11TextView = new TextView(this);
@@ -625,6 +645,7 @@ public class GritTestActivity extends AppCompatActivity {
         item11TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item11TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item11TextViewParams.addRule(RelativeLayout.BELOW, item10RadioGroup.getId());
+        item11TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item11TextView, item11TextViewParams);
 
         RadioGroup item11RadioGroup = new RadioGroup(this);
@@ -673,6 +694,7 @@ public class GritTestActivity extends AppCompatActivity {
         item11RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item11RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item11RadioGroupParams.addRule(RelativeLayout.BELOW, item11TextView.getId());
+        item11RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item11RadioGroup, item11RadioGroupParams);
 
         TextView item12TextView = new TextView(this);
@@ -683,6 +705,7 @@ public class GritTestActivity extends AppCompatActivity {
         item12TextView.setId(View.generateViewId());
         RelativeLayout.LayoutParams item12TextViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item12TextViewParams.addRule(RelativeLayout.BELOW, item11RadioGroup.getId());
+        item12TextViewParams.setMargins(0, 20, 0, 0);
         relativeLayout.addView(item12TextView, item12TextViewParams);
 
         RadioGroup item12RadioGroup = new RadioGroup(this);
@@ -731,6 +754,7 @@ public class GritTestActivity extends AppCompatActivity {
         item12RadioGroup.setId(View.generateViewId());
         RelativeLayout.LayoutParams item12RadioGroupParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         item12RadioGroupParams.addRule(RelativeLayout.BELOW, item12TextView.getId());
+        item12RadioGroupParams.setMargins(0, 10, 0, 0);
         relativeLayout.addView(item12RadioGroup, item12RadioGroupParams);
 
         scrollView.addView(relativeLayout);
