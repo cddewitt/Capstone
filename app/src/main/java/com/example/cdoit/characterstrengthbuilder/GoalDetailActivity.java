f0d014ebc03798d39b1ebe98ad015e1ca9b6b39d
cddewitt <cddewitt@bsu.edu>
2016-02-18 14:17:34 -0500

    finished working on goal detil activity. Can no mark a WOOP as completed

--- app/src/main/java/com/example/cdoit/characterstrengthbuilder/GoalDetailActivity.java
+++ app/src/main/java/com/example/cdoit/characterstrengthbuilder/GoalDetailActivity.java
@@ -1,5 +1,9 @@
 package com.example.cdoit.characterstrengthbuilder;
 
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -7,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,11 +25,13 @@ public class GoalDetailActivity extends AppCompatActivity {
     private TextView tbxTime;
     private SQLiteDatabase db;
     private int rowID;
+    private long dateCreated;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal_detail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Bundle extras = getIntent().getExtras();
         grabTextViews();
         String rowID = extras.getString("RowID");
@@ -39,6 +46,8 @@ public class GoalDetailActivity extends AppCompatActivity {
             tbxPlan.setText(cursor.getString(cursor.getColumnIndex(DatabaseContract.IncompleteGoals.COLUMN_PLAN)));
             tbxDate.setText(cursor.getString(cursor.getColumnIndex(DatabaseContract.IncompleteGoals.COLUMN_DEADLINE_DATE)));
             tbxTime.setText(cursor.getString(cursor.getColumnIndex(DatabaseContract.IncompleteGoals.COLUMN_DEADLINE_TIME)));
+            dateCreated=cursor.getLong(cursor.getColumnIndex(DatabaseContract.IncompleteGoals.COLUMN_DATE_CREATED));
+            setTitle(tbxWish.getText().toString());
         }
 
     }
@@ -65,7 +74,7 @@ public class GoalDetailActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.delete_goal) {
-            deleteGoal(this.rowID);
+            popUpBox();
             return true;
         }
         //noinspection SimplifiableIfStatement
@@ -76,9 +85,51 @@ public class GoalDetailActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void popUpBox(){
+        new AlertDialog.Builder(getApplicationContext())
+                .setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this WOOP?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteGoal(rowID);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
+    }
+
     private void deleteGoal(int id) {
         db.delete(DatabaseContract.IncompleteGoals.TABLENAME, DatabaseContract.IncompleteGoals.COLUMN_ID + "=" + id, null);
         Toast toast = Toast.makeText(getApplicationContext(), "Your WOOP was been deleted", Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    public void btnMarkAsCompletedClick(View v) {
+        ContentValues values = new ContentValues();
+        values.put(DatabaseContract.CompleteGoals.COLUMN_WISH,tbxWish.getText().toString());
+        values.put(DatabaseContract.CompleteGoals.COLUMN_OUTCOME,tbxOutcome.getText().toString());
+        values.put(DatabaseContract.CompleteGoals.COLUMN_OBSTACLE,tbxObstacles.getText().toString());
+        values.put(DatabaseContract.CompleteGoals.COLUMN_PLAN,tbxPlan.getText().toString());
+        values.put(DatabaseContract.CompleteGoals.COLUMN_DATE_COMPLETED,System.currentTimeMillis());
+        values.put(DatabaseContract.CompleteGoals.COLUMN_DATE_CREATED,dateCreated);
+        values.put(DatabaseContract.CompleteGoals.COLUMN_DEADLINE_TIME,tbxTime.getText().toString());
+        values.put(DatabaseContract.CompleteGoals.COLUMN_DEADLINE_DATE,tbxDate.getText().toString());
+        long id= db.insert(DatabaseContract.CompleteGoals.TABLENAME,null,values);
+        if(id==-1)
+        {
+            Toast toast =Toast.makeText(this.getApplicationContext(),"Failed to move WOOP to completed",Toast.LENGTH_LONG);
+            toast.show();
+        }
+        else {
+            deleteGoal(rowID);
+            Toast toast = Toast.makeText(this.getApplicationContext(), "WOOP successfully moved to completed", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
