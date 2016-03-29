6d22d16658b18ad9290a397dc59a5fc914841f3d
cddewitt <cddewitt@bsu.edu>
2016-03-28 22:46:42 -0400

    Merge branch 'master' of C:\Users\Cory\Desktop\capstone with conflicts.

--- app/src/main/java/com/example/cdoit/characterstrengthbuilder/ObstacleActivity.java
+++ app/src/main/java/com/example/cdoit/characterstrengthbuilder/ObstacleActivity.java
@@ -1,16 +1,21 @@
 package com.example.cdoit.characterstrengthbuilder;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ObstacleActivity extends AppCompatActivity {
 
-    private TextView tbxObstacles;
     private String characteristic = "";
     private String wish = "";
     private String outcome = "";
@@ -20,7 +25,6 @@ public class ObstacleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle("Character Strength Builder");
         setContentView(R.layout.woop_obstacle);
-        tbxObstacles = (TextView) findViewById(R.id.obstacleEditText);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             characteristic = extras.getString("Characteristic");
@@ -30,12 +34,13 @@ public class ObstacleActivity extends AppCompatActivity {
     }
 
     public void continueToPlanButtonClick(View v) {
-        Log.v("Obstacles", "in click");
-        String obstacles = tbxObstacles.getText().toString();
+        String obstacles =getObstacles();
         if (obstacles.equals("")) {
             Toast toast = Toast.makeText(getApplicationContext(), "Please enter a possible obstacle before continuing", Toast.LENGTH_LONG);
             toast.show();
         } else {
+            Toast toast = Toast.makeText(getApplicationContext(),obstacles,Toast.LENGTH_LONG);
+            toast.show();
             Intent intent = new Intent(this, PlanActivity.class);
             intent.putExtra("Characteristic", characteristic);
             intent.putExtra("Wish", wish);
@@ -45,4 +50,36 @@ public class ObstacleActivity extends AppCompatActivity {
         }
     }
 
+    public void addObstacleButtonClick(View v) {
+        EditText et =(EditText)findViewById(R.id.obstacleEditText);
+        Drawable draw = et.getBackground();
+        ViewGroup.LayoutParams lay = et.getLayoutParams();
+        lay.height=ViewGroup.LayoutParams.WRAP_CONTENT;
+        LinearLayout linearLayout=(LinearLayout)findViewById(R.id.containerObstacle);
+        EditText tv = new EditText(this);
+        tv.setHint("Enter an obstacle here...");
+        tv.setBackground(draw);
+        tv.setLayoutParams(lay);
+        linearLayout.addView(tv);
+    }
+
+    public String getObstacles(){
+        String answer="";
+        LinearLayout ll = (LinearLayout)findViewById(R.id.containerObstacle);
+       for(int i =0;i<ll.getChildCount();i++)
+       {
+           View view =ll.getChildAt(i);
+           if(view instanceof EditText)
+           {
+               TextView obstacle =(TextView)view;
+               if(!obstacle.getText().toString().equals(""))
+               {
+                   answer+=obstacle.getText().toString()+"~";
+               }
+           }
+       }
+        if(!answer.equals(""))
+        answer=answer.substring(0,answer.length()-1);
+        return answer;
+    }
 }
