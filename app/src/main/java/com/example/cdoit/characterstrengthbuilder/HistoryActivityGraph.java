8c21e853325a29338bc25eacc5b9643fe46f029b
TJGreen0211 <tjgreen@bsu.edu>
2016-04-12 12:56:09 -0400

    Worked on the graphs and implemented some functionality

--- app/src/main/java/com/example/cdoit/characterstrengthbuilder/HistoryActivityGraph.java
+++ app/src/main/java/com/example/cdoit/characterstrengthbuilder/HistoryActivityGraph.java
@@ -4,18 +4,21 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.series.BarGraphSeries;
+import android.widget.TextView;
 
 /**
  * Created by tylergreen on 4/7/16.
  */
 public class HistoryActivityGraph extends AppCompatActivity {
+    private TextView completedGRIP;
+    private TextView incompleteGRIP;
+
     private DataPoint[] generateDataPoints() {
-        int count = getGoals();
+        int count = 10;
         DataPoint[] values = new DataPoint[count];
         for(int i = 0; i < count; i++) {
             DataPoint v = new DataPoint(i, i*i);
@@ -26,22 +29,26 @@ public class HistoryActivityGraph extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("GRAPH");
         setContentView(R.layout.history_graph);
-        getGoals();
+        completedGRIP = (TextView)findViewById(R.id.numComplete);
+        completedGRIP.setText("Number of completed GRIPs: "+10);
+        incompleteGRIP = (TextView)findViewById(R.id.numIncomplete);
+        incompleteGRIP.setText("Number of completed GRIPs: "+10);
+        setTitle("GRAPH");
 
         GraphView line = (GraphView) this.findViewById(R.id.historyGraph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(generateDataPoints());
         line.addSeries(series);
 
         BarGraphSeries<DataPoint> series2 = new BarGraphSeries<>(generateDataPoints());
+        series2.setSpacing(50);
         line.addSeries(series2);
     }
 
     private int getGoals() {
         int completedGoals = 0;
         DatabaseHelper helper = new DatabaseHelper(getApplicationContext());
-        SQLiteDatabase db = helper.getWritableDatabase();
+        SQLiteDatabase db = helper.getReadableDatabase();
         Cursor cursor = db.query(DatabaseContract.IncompleteGoals.TABLENAME, null, null, null, null, null, null);
         try {
             if (cursor != null)//check to see if we got any result back
@@ -53,19 +60,6 @@ public class HistoryActivityGraph extends AppCompatActivity {
         } finally {
             cursor.close();
         }
-        getDate();
         return completedGoals;
-    }
-
-    private int getDate() {
-        long dateTime = 0;
-        DatabaseHelper helper = new DatabaseHelper(getApplicationContext());
-        SQLiteDatabase db = helper.getReadableDatabase();
-        Cursor cursor = db.query(DatabaseContract.IncompleteGoals.TABLENAME, null, null, null, null, null, null);
-        cursor.moveToFirst();
-        while(cursor.isAfterLast() == false) {
-            dateTime = cursor.getLong(2);
-        }
-        return 0;
     }
 }
