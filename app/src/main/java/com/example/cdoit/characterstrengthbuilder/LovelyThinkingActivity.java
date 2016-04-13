fbecf59307589d84991c684a7cea68263620555a
Jessica Stanford <jlstanford@bsu.edu>
2016-04-13 01:12:26 -0400

    Implemented hyperlink to lovelythinking.com/apps
    lovelythinking.com/apps is down as of 4/13/2016 at 1:12am EST and shows 404 error.

--- app/src/main/java/com/example/cdoit/characterstrengthbuilder/LovelyThinkingActivity.java
+++ app/src/main/java/com/example/cdoit/characterstrengthbuilder/LovelyThinkingActivity.java
@@ -1,11 +1,10 @@
 package com.example.cdoit.characterstrengthbuilder;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class LovelyThinkingActivity extends AppCompatActivity {
 
@@ -15,15 +14,10 @@ public class LovelyThinkingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lovely_thinking);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TextView view = (TextView)findViewById(R.id.lovelyTextView);
+        if (view != null) {
+            view.setMovementMethod(LinkMovementMethod.getInstance());
+        }
     }
 
 }
